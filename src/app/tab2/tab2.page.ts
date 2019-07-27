228b883c97c2f06dabdc97b7f5809f3f672a80e3
Unknown <aboniaa@gmail.com>
2019-07-27 17:48:47 +0200

    get quantity and check availability

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,11 +7,22 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
   today
+  public currentNumber = 1;
   date: string;
   type: 'string'; // 'string' | 'js-date' | 'moment' | 'time' | 'object'
   constructor() {
     this.today = new Date().toISOString();
   }
-
+  public increment() {
+    if(this.currentNumber<13){
+      this.currentNumber++;
+    }
+  }
+  
+  public decrement() {
+    if(this.currentNumber>1){
+      this.currentNumber--;
+    }
+  }
 
 }
